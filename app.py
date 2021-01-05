446a05c841040142c6bc96d397a3eca95e735bf5
yonikosiner <yoni@kosiner.co.uk>
2021-01-05 15:12:41 +0000

    cleaner code

--- app.py
+++ app.py
@@ -9,27 +9,31 @@ amountOfTimes = int(0)
 money = int(input("please enter money "))
 money = money
 looping = True
-numberGoes = int(0)
+numberGoes = int(1)
 
 while looping:
+    # Get random item from reel array and store it in a variole
     reel1 = choice(reel)
     reel2 = choice(reel)
     reel3 = choice(reel)
 
+    # Take one away from money variole
     money = money - 1
+    # Add one to the amount of times user has had a go
     amountOfTimes = amountOfTimes + 1
+    # Print the amount of goes you have had
     print("You have had (%d) attempts" % numberGoes)
-    print(amountOfTimes)
-    print(money)
+    # Print the results
     print(reel1 + " "+reel2+" "+reel3)
+    # Wait two seconds
     sleep(2)
     # Change the number of goes to plus one
     numberGoes = numberGoes + 1
     # Check if user has run out of money
-    if money == 1:
+    if money == 0:
         print("Sorry you have run out of money pay for more or play again later")
         looping = False
-
+    # If user gets all three slots in the row
     if reel1 == reel2 and reel1 == reel3:
         print("Jackpot")
         sleep(2)
